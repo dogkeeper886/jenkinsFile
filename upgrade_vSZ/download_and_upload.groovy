7b51a4f5bb80d6c1e838ce9f1bc265bbd46d1018
dogkeeper886 <jack.w.tseng@outlook.com>
2019-08-23 14:04:38 +0800

    Seperate pipeline

---
+++ upgrade_vSZ/download_and_upload.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+        stages {
+        stage('Download Image') {
+            input {
+                message "Input vSZ version"
+                ok "Apply"
+                parameters {
+                    string(name: 'szVer', defaultValue: '5.2.0.0.449', description: 'Apply to start pipeline')
+                }
+            steps {
+                sh 'python3 getImg.py $szVer'
+            }
+        }
+        stage('Uplod to FTP') {
+            environment {
+                ftpIp = '10.206.5.20'
+                BITBUCKET_COMMON_CREDS = credentials('ftp')
+            }
+            steps {
+                sh 'python3 ftpUpload.py $ftpIp $BITBUCKET_COMMON_CREDS_USR $BITBUCKET_COMMON_CREDS_PSW $szVer'
+            }
+        }
+    }
+}
