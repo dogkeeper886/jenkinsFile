d0f639b18b3e0e0a172efe7533afbd3090f0cefe
dogkeeper886 <jack.w.tseng@outlook.com>
2019-08-23 16:34:23 +0800

    Modify

--- upgrade_vSZ/download_and_upload.groovy
+++ upgrade_vSZ/download_and_upload.groovy
@@ -1,23 +1,35 @@
 pipeline {
     agent any
+    parameters {
+        string(name: 'szVer', defaultValue: '5.2.0.0.449', description: 'vsz ximg file version')
+        string(name: 'ftpIp', defaultValue: '10.206.5.20', description: 'ftp server ip')
+        string(name: 'szIp', defaultValue: '10.206.20.109', description: 'vsz ip address')
+    }
     stages {
+        stage('Setup Environment') {
+            steps {
+                sh 'python3 upgrade_vSZ/writeEnv.py szVer $szVer'
+            }
+        }
         stage('Download Image') {
             environment {
-                ftpIp = '10.206.5.20'
                 BITBUCKET_COMMON_CREDS = credentials('ftp')
-            }
-            input {
-                message "Input vSZ version"
-                ok "Apply"
-                parameters {
-                    string(name: 'szVer', defaultValue: '5.2.0.0.449', description: 'Apply to start pipeline')
-                }
-            }
+            }            
             steps {
                 echo 'Download start.'
                 sh 'python3 upgrade_vSZ/getXimg.py $szVer'
+            }
+        }
+        stage('Upload to FTP') {
                 echo 'Upload start'
                 sh 'python3 upgrade_vSZ/uploadXimg.py $ftpIp $BITBUCKET_COMMON_CREDS_USR $BITBUCKET_COMMON_CREDS_PSW $szVer'
+        }
+        stage('Upgrade') {
+            environment {
+                BITBUCKET_COMMON_CREDS = credentials('vsz')
+            }
+            steps {
+                sh 'python3 upgrade_vSZ/ia.py $szIp $BITBUCKET_COMMON_CREDS_USR $BITBUCKET_COMMON_CREDS_PSW $szVer'
             }
         }
     }
