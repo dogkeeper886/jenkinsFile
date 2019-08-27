35fcd5687ba5625aae3a5f724203e54f6462c9ce
dogkeeper886 <jack.w.tseng@outlook.com>
2019-08-27 10:16:21 +0800

    Add new jenkins pipeline

---
+++ rebuild_vSZ/getImg.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import urllib.request
+
+if len(sys.argv) != 2:
+    print('Invalid argument [SZ_VERSION]')
+    exit(1)
+
+def ximgInfo(szVer):
+    ximg = {
+        'fileUrl': 'http://tdc-repository.arrisi.com:8081/nexus/content/repositories/releases/ruckus/official/mega/sz/5.1.2.0/sz_512/' + szVer + '/vscg/' + 'vscg-' + szVer + '.ximg',
+        'fileName': 'vscg-' + szVer + '.ximg'
+    }
+    return ximg
+
+fileInfo = ximgInfo(sys.argv[1])
+
+def getImg():
+    try:
+        urllib.request.urlretrieve(fileInfo['fileUrl'], filename=fileInfo['fileName'])
+    except urllib.error.URLError as err:
+        print(err)
+        exit(1)
+
+if os.path.exists(fileInfo['fileName']): 
+    print('File exist')
+else:
+    getImg()
