46d70e9013ae3ddf1700f7372f6e8f1f6eb888da
dogkeeper886 <jack.w.tseng@outlook.com>
2019-08-28 17:08:11 +0800

    update

--- rebuild_vSZ/image_list.py
+++ rebuild_vSZ/image_list.py
@@ -9,8 +9,6 @@ apiAccess = {
     'fileName': 'vscg-' + os.environ['szVer'] + '.qcow2'
 }
 
-apiData = dict()
-
 # read token from file
 with open('token.json', 'rt') as fh:
     headers = json.load(fh)
@@ -31,6 +29,35 @@ def get(apiAccess, headers):
     return resp
 
 
+def post(apiAccess, headers, apiData):
+    apiUrl = apiAccess['endpoint'] + apiAccess['api']
+    try:
+        resp = requests.post(apiUrl, headers=headers, json=apiData)
+    except requests.exceptions.RequestException as err:
+        print(err)
+        exit(1)
+    print('Status:\n', resp.status_code)
+    print('Headers:\n', resp.headers)
+    print('Content:\n', resp.content)
+    resp.close()
+    return resp
+
+
+def postFile(apiAccess, headers, apiData):
+    apiUrl = apiAccess['endpoint'] + apiAccess['api']
+    try:
+        resp = requests.put(apiUrl, headers=headers,
+                            data=open(apiData['name'], 'rb'))
+    except requests.exceptions.RequestException as err:
+        print(err)
+        exit(1)
+    print('Status:\n', resp.status_code)
+    print('Headers:\n', resp.headers)
+    print('Content:\n', resp.content)
+    resp.close()
+    return resp
+
+
 # start
 resp = get(apiAccess, headers)
 imageList = resp.json()['images']
@@ -39,7 +66,22 @@ print(json.dumps(imageList, indent=4))
 nameList = list()
 for image in imageList:
     nameList.append(image['name'])
+
+
 if apiAccess['fileName'] in nameList:
     print('File exist. Skip')
 else:
     print('Upload start')
+    # create
+    apiData = {
+        'container_format': 'bare',
+        'disk_format': 'qcow2',
+        'name': apiAccess['fileName']
+    }
+    resp = post(apiAccess, headers, apiData)
+    # upload
+    apiAccess['api'] = resp.json()['file']
+    print(apiAccess['api'])
+    headers['Content-Type'] = 'application/octet-stream'
+    print(headers)
+    resp = postFile(apiAccess, headers, apiData)
