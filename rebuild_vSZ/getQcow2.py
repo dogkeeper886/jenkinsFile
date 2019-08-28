93c5703d47ba6826003732fe41388fc377fd02b3
dogkeeper886 <jack.w.tseng@outlook.com>
2019-08-28 16:26:49 +0800

    Update

--- rebuild_vSZ/getQcow2.py
+++ rebuild_vSZ/getQcow2.py
@@ -1,5 +1,5 @@
 import os
-import requests
+import urllib.request
 
 apiAccess = {
     'endpoint': 'http://tdc-repository.arrisi.com:8081/nexus/content/repositories/releases/ruckus/official/mega/sz',
@@ -8,22 +8,9 @@ apiAccess = {
 }
 
 
-def getQcow2(apiAccess):
-    apiUrl = apiAccess['endpoint'] + apiAccess['api']
-    try:
-        resp = requests.get(url=apiUrl, stream=True)
-    except requests.exceptions.RequestException as err:
-        print(err)
-        exit(1)
-    print('Status:\n', resp.status_code)
-    print('Headers:\n', resp.headers)
-    print('Content:\n', resp.content)
-    resp.close()
-    return resp
-
-
 # start
 if os.path.exists(apiAccess['fileName']):
     print('File exist. Skip')
 else:
-    resp = getQcow2(apiAccess)
+    apiUrl = apiAccess['endpoint'] + apiAccess['api']
+    urllib.request.urlretrieve(apiUrl, filename=apiAccess['fileName'])
