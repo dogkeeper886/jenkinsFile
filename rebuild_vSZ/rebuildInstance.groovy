1423ce0da80f8dfeb4d8338b72f9bfdec1393326
dogkeeper886 <jack.w.tseng@outlook.com>
2019-08-29 15:57:27 +0800

    update stage 2

--- rebuild_vSZ/rebuildInstance.groovy
+++ rebuild_vSZ/rebuildInstance.groovy
@@ -1,8 +1,5 @@
 pipeline {
     agent any
-    parameters {
-        string(name: 'szVer', defaultValue: '5.2.0.0.458', description: 'Input SZ version')        
-    }
     stages {
         stage('Login OpenStack') {
             environment {
@@ -11,6 +8,19 @@ pipeline {
             steps {
                 echo 'Login start'
                 sh 'python3 rebuild_vSZ/identity_auth_tokens.py'
+            }
+        }
+        stage() {
+            input {
+                message "Input parameter"
+                parameters {
+                    string(name: 'szVer', defaultValue: '5.2.0.0.458', description: 'Input SZ version')        
+                    choice(name: 'instanceName', choices: ['vSZinstance1', 'vSZinstance2', 'vSZinstance3', 'vSZinstance4'], description: 'Instance Name')
+                }
+            }
+            steps {
+                echo "$szVer"
+                echo "$choice"
             }
         }
     }
