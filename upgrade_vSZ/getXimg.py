c8145e2c9763f189ef5a2756a1776f6c2a4c8ffc
dogkeeper886 <jack.w.tseng@outlook.com>
2019-08-30 17:01:31 +0800

    move file

--- upgrade_vSZ/getXimg.py
+++ upgrade_vSZ/getXimg.py
@@ -1,28 +1,21 @@
-import sys
 import os
 import urllib.request
 
-if len(sys.argv) != 2:
-    print('Invalid argument [SZ_VERSION]')
-    exit(1)
+apiAccess = {
+    'endpoint': 'http://tdc-repository.arrisi.com:8081/nexus/content/repositories/releases/ruckus/official/mega/sz',
+    'api': '/5.2.0.0/ML/' + os.environ['szVer'] + '/vscg/vscg-' + os.environ['szVer'] + '.qcow2',
+    'fileName': 'vscg-' + os.environ['szVer'] + '.ximg'
+}
 
-def ximgInfo(szVer):
-    ximg = {
-        'fileUrl': 'http://tdc-repository.arrisi.com:8081/nexus/content/repositories/releases/ruckus/official/mega/sz/5.1.2.0/sz_512/' + szVer + '/vscg/' + 'vscg-' + szVer + '.ximg',
-        'fileName': 'vscg-' + szVer + '.ximg'
-    }
-    return ximg
 
-fileInfo = ximgInfo(sys.argv[1])
-
-def getImg():
+# start
+if os.path.exists(apiAccess['fileName']):
+    print('File exist. Skip')
+else:
+    apiUrl = apiAccess['endpoint'] + apiAccess['api']
     try:
-        urllib.request.urlretrieve(fileInfo['fileUrl'], filename=fileInfo['fileName'])
+        urllib.request.urlretrieve(apiUrl, filename=apiAccess['fileName'])
     except urllib.error.URLError as err:
         print(err)
         exit(1)
-
-if os.path.exists(fileInfo['fileName']): 
-    print('File exist')
-else:
-    getImg()
+    
