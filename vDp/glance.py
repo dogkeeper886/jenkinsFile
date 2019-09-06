233774f75b35dce3d8af1a06928df6e57727785e
dogkeeper886 <jack.w.tseng@outlook.com>
2019-09-06 15:08:23 +0800

    update

--- vDp/glance.py
+++ vDp/glance.py
@@ -14,7 +14,23 @@ keystone = keystoneclient.Client(
 glance_endpoint = keystone.service_catalog.url_for(service_type='image')
 glance = glclient.Client(glance_endpoint, token=keystone.auth_token)
 listGen = glance.images.list()
-imageList = list(listGen)
-#print(json.dumps(imageList, indent=4))
-for image in imageList:
-    print(image.name)
+info = list(listGen)
+imageList = list()
+for imageInfo in info:
+    imageList.append(imageInfo.name)
+print(imageList)
+
+fileName = 'vdp-' + env['szVer'] + '.qcow2'
+if fileName in imageList:
+    print('File exist', fileName)
+else:
+    print('File not found', fileName)
+    with open(fileName) as fimage:
+        glance.images.create(
+            name="myimage",
+            is_public=False,
+            disk_format="qcow2",
+            container_format="bare",
+            data=fimage
+        )
+
