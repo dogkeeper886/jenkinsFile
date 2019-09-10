d647f172cc4fca18ac608451692f8be06f32502a
dogkeeper886 <jack.w.tseng@outlook.com>
2019-09-10 12:12:27 +0800

    ansible step

--- vDp/rebuildsetup.groovy
+++ vDp/rebuildsetup.groovy
@@ -1,14 +1,18 @@
 pipeline {
     agent any
     stages {
-        stage('Rebuild vDP') {
-            
+        stage('Rebuild vDP') {           
             steps {
                 echo 'Rebuild start'
+                sh 'ansible -u root kvm -m virt -a "name=vdp state=shutdown"'
+                sh 'ansible -u root kvm -m virt -a "name=vdp command=status"'
+                sh 'ansible -u root kvm -m file -a "path=/var/lib/libvirt/images/vdp.qcow2 state=absent"'
+                sh 'ansible -u root kvm -m copy -a "remote_src=yes src=$WORKSPACE/vdp-$szVer.qcow2 dest=/var/lib/libvirt/images/vdp.qcow2"'
+                sh 'ansible -u root kvm -m virt -a "name=vdp state=running"'
+                sh 'ansible -u root kvm -m virt -a "name=vdp command=status"'
             }
-
         }
-        stage('Setup vDp') {
+        /*stage('Setup vDp') {
             environment {
                 BITBUCKET_COMMON_CREDS = credentials('kvm')
             }                        
@@ -16,6 +20,6 @@ pipeline {
                 echo 'Setup start'
                 sh 'python3 vDp/setupVdp.py'
             }
-        }
+        }*/
     }
 }
