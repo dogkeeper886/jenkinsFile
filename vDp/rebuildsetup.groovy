f991355a189de9f81875a16bb3ed61f108ddd236
dogkeeper886 <jack.w.tseng@outlook.com>
2019-09-10 16:21:48 +0800

    update

--- vDp/rebuildsetup.groovy
+++ vDp/rebuildsetup.groovy
@@ -26,7 +26,19 @@ pipeline {
             steps {
                 echo 'Rebuild start'
                 sh 'ansible -u root kvm -m virt -a "name=vdp state=shutdown"'
-                sh 'ansible -u root kvm -m virt -a "name=vdp command=status"'
+                //sh 'ansible -u root kvm -m virt -a "name=vdp command=status"'
+                retry(3) {
+                    try {
+                        sh 'python3 vDp/statusCheck.py'
+                    }
+                    catch (exc) {
+                        echo 'Something failed, I should sound the klaxons!'
+                        throw
+                    }
+                    finally {
+                        sleep 60
+                    }
+                }
                 sh 'ansible -u root kvm -m file -a "path=/var/lib/libvirt/images/vdp.qcow2 state=absent"'
                 sh 'ansible -u root kvm -m copy -a "remote_src=yes src=/var/lib/libvirt/images/vdp-$szVer.qcow2 dest=/var/lib/libvirt/images/vdp.qcow2"'
                 sh 'ansible -u root kvm -m virt -a "name=vdp state=running"'
--- vDp/statusCheck.py
+++ vDp/statusCheck.py
@@ -1,5 +1,14 @@
 import subprocess
-
+import re
 cmd = 'ansible -u root kvm -m virt -a "name=vdp command=status"'
 result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE)
 print(result.stdout.decode())
+
+text = result.stdout.decode()
+j = re.findall("\"status\": \"(.*)\"", text)
+
+if j[0] != 'shutdown':
+    print('Instance status is not shutdown')
+    exit(1)
+else:
+    print('Instance status is shutdown')
