d00a2d8abc8724be07aef057a5de45199842326f
dogkeeper886 <jack.w.tseng@outlook.com>
2019-09-10 14:43:57 +0800

    update

--- vDp/rebuildsetup.groovy
+++ vDp/rebuildsetup.groovy
@@ -2,9 +2,25 @@ pipeline {
     agent any
     parameters {
         string(name: 'szVer', defaultValue: '5.2.0.0.476', description: 'Input SZ version')
-        booleanParam(name: 'REBUILD', defaultValue: true, description: 'Rebuild instance')
+        booleanParam(name: 'REBUILD', defaultValue: false, description: 'Rebuild instance')
+        booleanParam(name: 'SETUP', defaultValue: false, description: 'Setup instance')
+
     }
     stages {
+        stage('Download file') {
+            when { not { expression { fileExists 'vdp-' + szVer + '.qcow2' } } }
+            steps {
+                echo 'File Download '
+                sh 'wget http://tdc-repository.arrisi.com:8081/nexus/content/repositories/releases/ruckus/official/mega/sz/5.2.0.0/ML/$szVer/vdp/vdp-$szVer.qcow2'
+                
+            }
+        }
+        stage('Copy image to KVM') {
+            steps {
+                echo 'Copy file vdp-' + szVer + '.qcow2 to KVM'
+                sh 'ansible -u root kvm -m copy -a "src=$WORKSPACE/vdp-$szVer.qcow2 dest=/var/lib/libvirt/images/"'
+            }
+        }
         stage('Rebuild vDP') {                                
             when { environment name: 'REBUILD', value: 'true' }
             steps {
@@ -17,14 +33,16 @@ pipeline {
                 sh 'ansible -u root kvm -m virt -a "name=vdp command=status"'
             }
         }
-        /*stage('Setup vDp') {
+        stage('Setup vDp') {
             environment {
                 BITBUCKET_COMMON_CREDS = credentials('kvm')
-            }                        
+            }
+            when { environment name: 'SETUP', value: 'true' }
+
             steps {
                 echo 'Setup start'
                 sh 'python3 vDp/setupVdp.py'
             }
-        }*/
+        }
     }
 }
