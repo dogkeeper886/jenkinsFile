c76c85d138ddf517c33c07f0ec501004749f7d01
dogkeeper886 <jack.w.tseng@outlook.com>
2019-09-10 11:48:43 +0800

    add stage

--- vDp/downloadImage.groovy
+++ vDp/downloadImage.groovy
@@ -13,30 +13,10 @@ pipeline {
         stage('Copy Image to kvm') {
             steps {
                 sh 'ansible -u root kvm -m ping'
+                sh 'echo "Start file vdp-$szVer.qcow2 to KVM"'
+                sh 'printenv'
+                sh 'ansible -u root kvm -m copy -a "src=$WORKSPACE/vdp-$szVer.qcow2 dest=/var/lib/libvirt/images/"'
             }
-
         }
-        /*stage('Upload Image to OpenStack') {
-            environment {
-                BITBUCKET_COMMON_CREDS = credentials('openstack')
-                //OS_USERNAME = BITBUCKET_COMMON_CREDS_USR
-                //OS_PASSWORD = BITBUCKET_COMMON_CREDS_PSW
-                OS_PROJECT_NAME = 'lab'
-                OS_USER_DOMAIN_NAME = 'Default'
-                OS_PROJECT_DOMAIN_NAME = 'Default'
-                OS_AUTH_URL = 'http://10.206.6.112:5000/v3'
-                OS_IDENTITY_API_VERSION = '3'
-            }
-            steps {
-                echo 'Login start'
-                sh 'python3 vDp/glance.py'
-            }
-        }*/
-        /*stage('Image Upload') {
-            steps{
-                echo 'Upload start'
-                sh 'python3 rebuild_vSZ/image_list.py'
-            }
-        }*/
     }
 }
