3dfe4d063802164d905385d38eaf5686cceca031
dogkeeper886 <jack.w.tseng@outlook.com>
2020-02-17 12:26:46 +0800

    46

--- setupVsz/setupVsz.groovy
+++ setupVsz/setupVsz.groovy
@@ -3,17 +3,29 @@ pipeline {
     parameters {
         string(name: 'vszIp', defaultValue: 'MY_IP', description: 'Input vSZ ip')
         string(name: 'vszProfile', defaultValue: 'MY_PROFILE', description: '1 for essential 2 for high scale')
-        string(name: 'vszIpMode', defaultValue: 'MY_IPMODE', description: '1 for ipv4 2 for ipv6')
-        string(name: 'vszIpType', defaultValue: 'MY_IPTYPE', description: '1 for manual 2 for dhcp')
+        string(name: 'vszIpMode', defaultValue: '1', description: '1 for ipv4 2 for ipv6')
+        string(name: 'vszIpType', defaultValue: '2', description: '1 for manual 2 for dhcp')
+        string(name: 'vszClusterName', defaultValue: 'MY_CLUSTE', description: 'Cluter Name')
+        string(name: 'vszConDes', defaultValue: 'MY_DES', description: 'Controller description')
+        string(name: 'vszNatIp', defaultValue: 'MY_NATIP', description: 'Nat ip address')
     }
     stages {
-        stage('Setup start') {
+        stage('Setup1 start') {
             environment {
                 BITBUCKET_COMMON_CREDS = credentials('vsz')
             }
             steps {
                 echo 'This is setup start'
                 sh 'python3 setupVsz/setupVsz.py'
+            }
+        }
+        stage('Setup2 start') {
+            environment {
+                BITBUCKET_COMMON_CREDS = credentials('vsz')
+            }
+            steps {
+                echo 'This is setup start'
+                sh 'python3 setupVsz/setupVsz2.py'
             }
         }
     }
---
+++ setupVsz/setupVsz2.py
@@ -0,0 +1,51 @@
+import pexpect
+import sys
+from os import environ
+
+
+class setupVsz:
+    def __init__(self, vszIp):
+        self.vszIp = vszIp
+        self.conn = pexpect.spawn(
+            'ssh admin@' + self.vszIp, encoding='utf-8', logfile=sys.stdout)
+        self.conn.timeout = 60
+        self.conn.expect('password:')
+        self.conn.sendline('admin')
+        self.conn.expect('>')
+        self.conn.sendline('enable')
+        self.conn.expect('Password:')
+        self.conn.sendline('admin')
+        self.conn.expect('#')
+
+    def __del__(self):
+        self.conn.sendline('logout')
+        self.conn.expect(pexpect.EOF)
+
+    def cluster(self, clusterName, conDes):
+        self.conn.sendline('setup')
+        self.conn.expect('(y/n)')
+        self.conn.sendline('n')
+        self.conn.expect('(c/j)')
+        self.conn.sendline('c')
+        self.conn.expect('Cluster Name')
+        self.conn.sendline(clusterName)
+        self.conn.expect('Controller Description')
+        self.conn.sendline(conDes)
+        self.conn.expect('(y/n)')
+        self.conn.sendline('y')
+
+    def nat(self, natIp):
+        pass
+        self.conn.expect('(y/n)')
+        self.conn.sendline('y')
+        self.conn.expect('NAT IP')
+        self.conn.sendline(natIp)
+        self.conn.expect('NTP Serve')
+        self.conn.sendline('tock.stdtime.gov.tw')
+        self.conn.expect('(y/n)')
+        self.conn.sendline('n')
+
+
+vsz = setupVsz(environ['vszIp'])
+vsz.cluster(environ['vszClusterName'], environ['vszConDes'])
+vsz.nat(environ['vszNatIp'])
